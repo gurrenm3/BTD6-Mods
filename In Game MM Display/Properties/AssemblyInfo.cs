a9becdbbb55b9b653e71ee92ae16a9a135d79588
Thomas <thomaspmar@protonmail.com>
2020-12-24 22:12:49 -0800

    Update AssemblyInfo.cs

--- In Game MM Display/Properties/AssemblyInfo.cs
+++ In Game MM Display/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("In_Game_MM_Display")]
+[assembly: AssemblyTitle("In Game MM Display")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("In_Game_MM_Display")]
+[assembly: AssemblyProduct("In Game MM Display")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
