b37cea21218150794489fb0abe7d26391154e581
Thomas <thomaspmar@protonmail.com>
2020-12-27 12:43:01 -0800

    Finished Random Heros

--- Random Hero/Patches/InGame_StartMatch.cs
+++ Random Hero/Patches/InGame_StartMatch.cs
@@ -2,6 +2,9 @@
 using Assets.Scripts.Unity.UI_New.InGame;
 using Assets.Scripts.Unity;
 using Gurren_Core.Extensions;
+using Random_Hero.Extensions;
+using System;
+using System.Collections.Generic;
 
 namespace Random_Hero.Patches
 {
@@ -9,9 +12,35 @@ namespace Random_Hero.Patches
     internal class InGame_StartMatch
     {
         [HarmonyPostfix]
-        internal static void Postfix()
+        internal static void Postfix(InGame __instance)
         {
-            
+            var profile = Game.instance.GetProfileModel();
+            List<string> unlockedHeros = new List<string>();
+
+            foreach (var hero in profile.unlockedHeroes)
+            {
+                if (!unlockedHeros.Contains(hero))
+                    unlockedHeros.Add(hero);
+            }
+
+            Random rand = new Random();
+            var randomHeroIndex = rand.Next(unlockedHeros.Count);
+
+            for (int i = 0; i < unlockedHeros.Count; i++)
+            {
+                var towerModel = Game.instance.GetTowerModel(unlockedHeros[i]);
+                if (towerModel is null)
+                    continue;
+
+                var detailModel = towerModel.GetTowerDetailsModel();
+                if (detailModel is null)
+                    continue;
+
+                detailModel.towerCount = (i == randomHeroIndex) ? 1 : 0;
+            }
+
+            var towerInventory = TowerInventory_Init.towerInventory;
+            towerInventory.SetTowerMaxes(TowerInventory_Init.allTowers);
         }
     }
 }
