8ca324d958287510a4bfc1f01ae048488b9d430c
Thomas <thomaspmar@protonmail.com>
2020-12-28 12:01:36 -0800

    Towers Randomly Sell

---
+++ Towers Randomly Sold/SessionData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Towers_Randomly_Sold
+{
+    class SessionData
+    {
+        public static TowerSellManager towerSellManager;
+    }
+}
